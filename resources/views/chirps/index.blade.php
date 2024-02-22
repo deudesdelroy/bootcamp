7c0e9ea5fbaf3843fec1cd4861c63d3186913af5
deudesdelroy <deudes15@hotmail.com>
2024-02-22 14:08:17 -0500

    feat: consult the chirps

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -22,6 +22,31 @@
                     </form>
                 </div>
             </div>
+            
+            <div class="mt-6 bg-white dark:bg-gray-800 shadow-sm rounded-lg divide-y dark:divide-gray-900">
+
+                {{-- chirps --}}
+                @foreach ($chirps as $chirp)
+                    <div class="p-6 flex space-x-2">
+
+                        <svg class="h-6 w-6 text-gray-600 dark:text-gray-400 -scale-x-100" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"></path>
+                        </svg>
+
+                        <div class="flex-1">
+                            <div class="flex justify-between items-center">
+                                <div>
+                                    <span class="text-gray-800 dark:text-gray-200">{{ $chirp->user->name }}</span>
+                                    <small class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ $chirp->created_at }}</small>
+                                </div>
+                            </div>
+                            <p class="mt-4 text-lg text-gray-900 dark:text-gray-100">{{ $chirp->message }}</p>
+                        </div>
+
+                    </div>
+                @endforeach
+
+            </div>
         </div>
     </div>
 </x-app-layout>
